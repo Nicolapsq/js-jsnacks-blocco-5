34879887a6b258682d72ea40f1b8b2947fbef1f0
Nicolapsq <nicolapasqua84@gmail.com>
2025-04-22 14:47:59 +0200

    Add-snack-1

---
+++ snack-1/script.js
@@ -0,0 +1,9 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+console.log(names);
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const nomiSingoli = names.map(nome => nome);
+
+console.log(...nomiSingoli);
